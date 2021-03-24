50683e94034ded96c979b5ec1804b6432d011198
ChristopherAChase <Christopher.A.Chase@outlook.com>
2021-03-24 11:21:47 -0500

    Refactored some CSS to work with react instead of static site

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,20 +4,23 @@ import '../styles/select.css'
 
 export default function Select(props) {
     return (
-        <div>
-            <img src={logo4} />
-            <h1>Welcome</h1>
-            <form>
-                <p>Enter school login</p>
-                <label className="logLabel">Username: </label>
-                <input type="text" id="username" className="textbox" placeholder="School Username" />
-                <br />
-                <label className="logLabel">Password:</label>
-                <input type="password" id="password" className="textbox" placeholder="Password" />
-                <br />
-                <input type="button" id="cancel" onClick={() => props.buttonPress('select')} value="Back" />
-                <input type="submit" id="submit" name="submit" value="Go!" />
-            </form>
+        <div className="selectPage">
+            <div className="selectBody">
+                <img className="circleImage" src={logo4} />
+                <h1 className="selectHeader">Welcome</h1>
+                <form onSubmit={(e) => e.preventDefault()}>
+                    <p>Enter school login</p>
+                    <label className="logLabel">Username: </label>
+                    <input type="text" id="username" className="textbox" placeholder="School Username" required />
+                    <br />
+                    <label className="logLabel">Password:</label>
+                    <input type="password" id="password" className="textbox" placeholder="Password" required />
+                    <br />
+                    <input type="button" id="cancel" onClick={() => props.changePage('select')} value="Back" />
+                    <input type="submit" id="submit" name="submit" value="Go!" onClick={() => props.changePage('assignments')} />
+                </form>
+            </div>
+
         </div>
     )
 }
